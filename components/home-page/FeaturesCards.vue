ec0c447e1b52e1b8cc34c6446e61343d5cbdd1cc
heshamabdulzaher <heshamabdulzaher@gmail.com>
2019-10-23 09:54:22 +0200

    test

--- components/home-page/FeaturesCards.vue
+++ components/home-page/FeaturesCards.vue
@@ -9,16 +9,14 @@
       <h3>أنشئ تصويتات لا محدودة بشكل مجاني 100٪</h3>
       <p>تساعدك هذه الأداة بإنشاء ومشاركة التصويتات والإستطلاعات بأسهل وأسرع طريقة ممكنة.</p>
     </div>
-    <a
-      href="'whatsapp://send?text=شارك بالتصويت' + '%0a %0a' + 'http://localhost:3000/stats/16unne'"
-    >WhatsApp</a>
+    <a href="'whatsapp://send?text=شارك بالتصويت %0a %0a https://poll-house.netlify.com/">WhatsApp</a>
     <a
       href="https://www.facebook.com/sharer.php?u=https://poll-house.netlify.com/"
       target="_blank"
     >Facebook</a>
     <a
       target="_blank"
-      href="'https://twitter.com/intent/tweet?text=شارك بالتصويت' + '%0a %0a' + 'http://localhost:3000/stats/16unne' + '&url=' + 'http://localhost:3000/stats/16unne'"
+      href="https://twitter.com/intent/tweet?text=شارك بالتصويت %0a %0a&amp;&url=https://poll-house.netlify.com/"
     >twitter</a>
   </section>
 </template>
